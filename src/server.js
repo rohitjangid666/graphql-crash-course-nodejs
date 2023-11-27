6d2ea67f863bcb8bd8f48b792220322adddb492e
Rohit Jangid <rohitjangid136@gmail.com>
2023-11-27 21:42:58 +0530

    setup

--- src/server.js
+++ src/server.js
@@ -1,1 +1,8 @@
-console.log('NodeJs initialized')
+import dotenv from 'dotenv'
+
+// initialize env
+dotenv.config({
+  path: './.env',
+})
+
+console.log('Node.js server will start on port:', process.env.PORT)
